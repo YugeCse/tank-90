71bad72603598d6e9ac1ed3da21fb03cc9013e82
Hwang <yugecosecant@foxmail.com>
2025-11-07 07:55:23 +0800

    #更新玩家生成

--- common/global_events.gd
+++ common/global_events.gd
@@ -5,3 +5,22 @@ signal on_player_tank_dead
 
 ## 敌方坦克死亡
 signal on_enemy_tank_dead(type: Enums.TankType)
+
+
+func emit_player_tank_dead_event():
+	on_player_tank_dead.emit()
+
+func on_player_tank_dead_connect(callable: Callable):
+	on_player_tank_dead.connect(callable)
+
+func on_player_tank_dead_disconnect(callable: Callable):
+	on_player_tank_dead.disconnect(callable)
+	
+func emit_enemy_tank_dead_event(type: Enums.TankType):
+	on_enemy_tank_dead.emit(type)
+
+func on_enemy_tank_dead_connect(callable: Callable):
+	on_enemy_tank_dead.connect(callable)
+	
+func on_enemy_tank_dead_disconnect(callable: Callable):
+	on_enemy_tank_dead.disconnect(callable)
--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -4,18 +4,33 @@ extends Node2D
 @export
 var warMap: WarMap
 
+@export
+var playerTankScene: PackedScene
+
 func _ready() -> void:
 	GlobalConfig.reset()
-	GlobalEvents.on_player_tank_dead\
-		.connect(_on_player_tank_dead)
-	GlobalEvents.on_enemy_tank_dead\
-		.connect(_on_enemy_tank_dead)
+	GlobalEvents.on_enemy_tank_dead_connect(_on_enemy_tank_dead)
+	GlobalEvents.on_player_tank_dead_connect(_on_player_tank_dead)
+	generate_player_tank() #生成玩家坦克
 
 func set_new_stage(stageLevel: int):
 	print('', stageLevel)
 
+## 添加到游戏相机范围
+func add_child_to_camera(child: Node):
+	$Camera2D.add_child(child)
+
+## 生成玩家坦克
+func generate_player_tank():
+	var tank = playerTankScene.instantiate() as PlayerTank
+	tank.position = Vector2(\
+		Constants.DESIGN_CX - 2 * Constants.WAR_SPRITE_SIZE, \
+		Constants.DESIGN_CY + Constants.WAR_MAP_SIZE / 2.0 - Constants.WAR_SPRITE_SIZE / 2.0)
+	add_child_to_camera(tank)
+
 func _on_player_tank_dead():
 	print('玩家被消灭')
+	generate_player_tank()
 
 func _on_enemy_tank_dead(type: Enums.TankType):
 	print('敌方坦克被消灭: ', type)
